4f033f14537fc05a46291e64f556ce6b4f29308c
Dashrath <devchaudhary0903@gmail.com>
2026-03-24 23:12:00 +0530

    Enhance evenOdd function with explanatory comments
    Added comments to explain bit manipulation for even/odd check.

--- day-02/script.js
+++ day-02/script.js
@@ -1,6 +1,8 @@
 // Problem 1: Check if a number is even or odd using bit manipulation
 function evenOdd(n){
-  if((n & 1) === 0){
+  if((n & 1) === 0){                         //& is the bitwise AND operator
+                                             //It compares each bit of n with 1
+                                            // the least significant bit matters her
     return "Even";
   } else if (n & 1 === 1){
     return "Odd"
